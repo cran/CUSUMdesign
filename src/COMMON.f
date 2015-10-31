39dce4f4818d915f49fdfd9f275eb9f49d38c65c
Boxiang Wang <boxiang@umn.edu>
2015-10-31 08:46:55 +0000

    version 1.1.1

---
+++ src/COMMON.f
@@ -0,0 +1,13 @@
+      SUBROUTINE CMPAR(R)
+      COMMON /PARMS/ A, B
+      DOUBLE PRECISION :: A, R
+      INTEGER :: B
+      A = R
+      END
+
+      SUBROUTINE CMPAR2(I)
+      COMMON /PARMS/ A, B
+      DOUBLE PRECISION :: A
+      INTEGER :: B, I
+      B = I
+      END
